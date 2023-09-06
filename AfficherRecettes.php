05c3762453bb62afc016dfeba9e6bd9aac0c99a9
cp069212 <Clement_Papillon@etu.u-bourgogne.fr>
2023-09-06 11:46:20 +0200

    Les fonctions

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -19,9 +19,63 @@ $recipes = [
         'recipe' => 'Etape 1 : la viande, ...',
         'author' => 'mathieu.nebra@exemple.com',
         'is_enabled' => true,
-    ]
+    ],
+    [
+        'title' => 'Salade Romaine',
+        'recipe' => '',
+        'author' => 'laurene.castor@exemple.com',
+        'is_enabled' => false,
+    ],
 ];
 
+$users = [
+    [
+        'full_name' => 'Mickaël Andrieu',
+        'email' => 'mickael.andrieu@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+
+function isValidRecipe(array $recipe) : bool {
+    if (array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+    return $isEnabled;
+}   
+
+function getRecipes(array $recipes) : array {
+    $validRecipes = [];
+    foreach($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $validRecipes[] = $recipe;
+        }
+    }
+    return $validRecipes;
+}
+
+function displayAuthor(string $authorEmail, array $users) : string
+{
+    for ($i = 0; $i < count($users); $i++) {
+        $author = $users[$i];
+        if ($authorEmail === $author['email']) {
+            return $author['full_name'] . '(' . $author['age'] . ' ans)';
+        }
+    }
+}
+
+$validRecipes = getRecipes($recipes);
 
 ?>
 
@@ -50,16 +104,24 @@ $recipes = [
 </style>    
 </head>
 <body>
+    <h1>Liste des recettes de cuisine</h1>
     <ul>
-    <?php foreach($recipes as $recipe): ?>
-        <?php if($recipe['is_enabled']) : ?>
+        <?php foreach ($validRecipes as $recipe): ?>
+        <?php $authorName = displayAuthor($recipe['author'], $users); ?>
+        <?php if ($authorName !== null): ?>
             <li>
-                <h2><?php echo $recipe['title'];?></h2>
-                <p><?php echo $recipe['recipe'];?></p>
-                <p><?php echo $recipe['author'];?></p>
-            </li> 
+                <h2><?php echo $recipe['title']; ?></h2>
+                <p><?php echo $recipe['recipe']; ?></p>
+                <p><?php echo $authorName; ?></p>
+            </li>
+        <?php else: ?>
+            <li>
+                <h2><?php echo $recipe['title']; ?></h2>
+                <p><?php echo $recipe['recipe']; ?></p>
+                <p><?php echo 'Auteur Inconnu'; ?></p>
+            </li>
         <?php endif; ?>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
     </ul>
 </body>
 </html>
