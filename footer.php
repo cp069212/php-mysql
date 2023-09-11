6cddee5a7c488c2cd07da41188ddfdc52ff0f4d2
cp069212 <Clement_Papillon@etu.u-bourgogne.fr>
2023-09-11 14:53:21 +0200

    Projet fil rouge

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
-<nav id="pied_de_page">
-    <div class="element_menu">
-        <ul>
-            <a>Copyright moi, tous droits réservés</a>
-        </ul>
-    </div>
-</nav>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Site de recettes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Copyright Clément Papillon, tous droits réservés</a>
+        </div>
+    </nav>
+</body>
---
+++ variables.php
@@ -0,0 +1,47 @@
+<?php
+// Déclaration du tableau des recettes
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : les flageolets, ...',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : la semoule, ...',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : la viande, ...',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Salade Romaine',
+        'recipe' => '',
+        'author' => 'laurene.castor@exemple.com',
+        'is_enabled' => false,
+    ],
+];
+
+$users = [
+    [
+        'full_name' => 'Mickaël Andrieu',
+        'email' => 'mickael.andrieu@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+?>
